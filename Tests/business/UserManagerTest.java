dd7b19d52396a7a72dced38f6a5595c458557fde
Sam <samuelakisanmi7@gmail.com>
2024-04-01 00:57:17 +0100

    tests,protocol,servers

---
+++ Tests/business/UserManagerTest.java
@@ -0,0 +1,20 @@
+package business;
+
+import junit.framework.TestCase;
+
+public class UserManagerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAddUser() {
+
+    }
+
+    public void testSearchByUsername() {
+    }
+}
