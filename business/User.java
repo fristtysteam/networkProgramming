00b8a3e2913f5bf2b73a96e5daee6d0e22799c0f
Marco <fristtysteam@gmail.com>
2024-03-31 16:53:07 +0100

    user class

---
+++ business/User.java
@@ -0,0 +1,54 @@
+public class User{
+    private String username;
+    private String password;
+    private boolean AdminStatus;
+    public User(){
+
+    }
+    public User(String username, String password, boolean adminStatus){
+        this.password = password;
+        this.username = username;
+        this.AdminStatus = adminStatus;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdminStatus() {
+        return AdminStatus;
+    }
+
+    public void setAdminStatus(boolean adminStatus) {
+        AdminStatus = adminStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getUsername(), getPassword(), isAdminStatus());
+    }
+}
